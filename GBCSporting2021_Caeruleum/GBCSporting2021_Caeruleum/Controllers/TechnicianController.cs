f29f5899788db3d1b1890f3599e596317cc68b30
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2021-02-23 00:22:37 -0500

    Created razor view pages and linked with asp action, created controllers for each component and views connecting to.

---
+++ GBCSporting2021_Caeruleum/GBCSporting2021_Caeruleum/Controllers/TechnicianController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GBCSporting2021_Caeruleum.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Technician()
+        {
+            ViewData["Message"] = "Technicians Page";
+            return View();
+        }
+    }
+}
